2c6e3d312926c56b1fadf298aa59770fcff94b45
Lisa Avdeenko <avdeenko.l26@mail.ru>
2023-10-03 00:13:49 +0300

    feat: add empty page when no results and location is '\results'

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -1,10 +1,14 @@
 import { useTypedSelector } from '../store/hooks';
-import { Typography } from 'antd';
+import { Button, Empty, Typography } from 'antd';
 import MonthResultDisplay from '../components/MonthResultDisplay';
 import { calcMonthNames } from '../common';
+import { useNavigate } from 'react-router-dom';
 
 const ResultsPage = () => {
-  const { result } = useTypedSelector((store) => store.resultData);
+  const navigate = useNavigate();
+  const { result, isCalculated } = useTypedSelector(
+    (store) => store.resultData
+  );
 
   const { DATA } = useTypedSelector(
     (store) => store.inputData.data.DataMonthInfo
@@ -14,54 +18,66 @@ const ResultsPage = () => {
   );
   const monthNames = calcMonthNames(FirstMonth, CountMonth);
 
-  return (
-    <>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          columnGap: '10px',
-          position: 'relative',
-          width: 'max-content',
-          height: '100%',
-          overflow: 'auto',
-        }}>
+  if (!isCalculated) {
+    return (
+      <Empty
+        image='https://gw.alipayobjects.com/zos/antfincdn/ZHrcdLPrvN/empty.svg'
+        imageStyle={{ height: 400 }}
+        description={<span>Рассчёты не обнаружены</span>}>
+        <Button type='primary' onClick={() => navigate('/')}>
+          Ввести исходные данные
+        </Button>
+      </Empty>
+    );
+  } else
+    return (
+      <>
         <div
           style={{
             display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            rowGap: '14px',
-            backgroundColor: 'white',
-            borderRadius: '8px',
-            padding: '16px',
+            flexDirection: 'row',
+            columnGap: '10px',
+            position: 'relative',
+            width: 'max-content',
+            height: '100%',
+            overflow: 'auto',
           }}>
-          <Typography.Title level={4}>
-            <span style={{ color: 'rgba(0, 0, 0, 45%)' }}>
-              Математические модели расчета объемов запасов{' '}
-            </span>
-            {' с учетом и без учета наложения '}
-            <span style={{ color: 'rgba(0, 0, 0, 45%)' }}>
-              времени работы дополнительных машин на смежных операциях
-            </span>
-          </Typography.Title>
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              rowGap: '14px',
+              backgroundColor: 'white',
+              borderRadius: '8px',
+              padding: '16px',
+            }}>
+            <Typography.Title level={4}>
+              <span style={{ color: 'rgba(0, 0, 0, 45%)' }}>
+                Математические модели расчета объемов запасов{' '}
+              </span>
+              {' с учетом и без учета наложения '}
+              <span style={{ color: 'rgba(0, 0, 0, 45%)' }}>
+                времени работы дополнительных машин на смежных операциях
+              </span>
+            </Typography.Title>
 
-          {result.map((item, i) => (
-            <MonthResultDisplay
-              key={i}
-              monthData={item}
-              initialData={DATA[i]}
-              monthName={monthNames[i]}
-            />
-          ))}
+            {result.map((item, i) => (
+              <MonthResultDisplay
+                key={i}
+                monthData={item}
+                initialData={DATA[i]}
+                monthName={monthNames[i]}
+              />
+            ))}
 
-          {/* <Typography>
+            {/* <Typography>
         <pre>{JSON.stringify(result, null, 2)}</pre>
       </Typography> */}
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
 };
 
 export default ResultsPage;
